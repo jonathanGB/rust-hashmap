a60d32ae1393253733520574f9d0738ba6e6e9d5
jonathanGB <jonathan.guillotte.blouin@gmail.com>
2019-06-05 23:15:18 -0400

    added remove and iterators to hashmap

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,30 @@
 use std::collections::hash_map::DefaultHasher;
 use std::hash::{Hash, Hasher};
 use std::mem;
+use std::iter::Iterator;
+use std::iter::IntoIterator;
 
 pub struct HashMap {
-  pub values: Vec<Option<String>>,
+  values: Vec<Option<(String, String)>>,
   len: usize,
   cap: usize,
+}
+
+pub struct HashMapIter<'a> {
+  hashmap: &'a HashMap,
+  at: usize,
 }
 
 impl HashMap {
   pub fn new() -> Self {
     const LEN: usize = 1000;
-    let arr : Vec<Option<String>> = vec![None; LEN];
-    HashMap{values: arr, len: 0, cap: LEN}
+    let arr : Vec<Option<(String, String)>> = vec![None; LEN];
+
+    HashMap{
+      values: arr,
+      len: 0,
+      cap: LEN,
+    }
   }
 
   fn hash(&self, key: &str) -> usize {
@@ -24,25 +36,71 @@ impl HashMap {
   pub fn insert(&mut self, key: String, val: String) -> Option<String> {
     let idx = self.hash(&key);
 
-    let existing_val = mem::replace(&mut self.values[idx], Some(val));
-    if let None = existing_val {
-      self.len += 1;
+    match mem::replace(&mut self.values[idx], Some((key, val))) {
+      Some((_, existing_val)) => Some(existing_val),
+      None => {
+        self.len += 1;
+        None
+      }
     }
-
-    existing_val
   }
 
-  pub fn get(&mut self, key: &str) -> &Option<String> {
+  pub fn get(&self, key: &str) -> Option<&String> {
     let idx = self.hash(key);
-    &self.values[idx]
+
+    match &self.values[idx] {
+      Some((_, val)) => Some(val),
+      None => None,
+    }
   }
 
-  pub fn contains_key(&mut self, key: &str) -> bool {
+  pub fn contains_key(&self, key: &str) -> bool {
     self.get(key).is_some()
   }
 
   pub fn len(&self) -> usize {
     self.len
+  }
+
+  pub fn remove(&mut self, key: &str) -> Option<String> {
+    let idx = self.hash(key);
+
+    match mem::replace(&mut self.values[idx], None) {
+      Some((_, removed_val)) => {
+        self.len -= 1;
+        Some(removed_val)
+      },
+      None => None
+    }
+  }
+}
+
+impl<'a> Iterator for HashMapIter<'a> {
+  type Item = (&'a String, &'a String);
+
+  fn next(&mut self) -> Option<Self::Item> {
+    for i in self.at..self.hashmap.cap {
+      let pair = &self.hashmap.values[i];
+      self.at += 1;
+
+      if let Some((key, val)) = pair {
+        return Some((key, val));
+      }
+    }
+
+    None
+  }
+}
+
+impl<'a> IntoIterator for &'a HashMap {
+  type Item = (&'a String, &'a String);
+  type IntoIter = HashMapIter<'a>;
+
+  fn into_iter(self) -> Self::IntoIter {
+    HashMapIter {
+      hashmap: &self,
+      at: 0,
+    }
   }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+//use std::collections::HashMap;
 use hashmap::HashMap;
 
 fn main() {
@@ -8,23 +9,53 @@ fn main() {
         "Adventures of Huckleberry Finn".to_string(),
         "My favorite book.".to_string(),
     );
+    println!("Length: {}", book_reviews.len());
+
     book_reviews.insert(
         "Grimms' Fairy Tales".to_string(),
         "Masterpiece.".to_string(),
     );
+    println!("Length: {}", book_reviews.len());
+
     book_reviews.insert(
         "Pride and Prejudice".to_string(),
         "Very enjoyable.".to_string(),
     );
+    println!("Length: {}", book_reviews.len());
+
     book_reviews.insert(
         "The Adventures of Sherlock Holmes".to_string(),
         "Eye lyked it alot.".to_string(),
     );
+    println!("Length: {}", book_reviews.len());
+
+    book_reviews.insert(
+        "Pride and Prejudice".to_string(), 
+        "Not very enjoyable".to_string()
+    );
+    println!("Length: {}", book_reviews.len());
+
 
     if !book_reviews.contains_key("Les Misérables") {
         println!("We've got {} reviews, but Les Misérables ain't one.",
              book_reviews.len());
     }
 
-    
+    book_reviews.remove("The Adventures of Sherlock Holmes");
+    println!("Length: {}", book_reviews.len());
+
+    let to_find = ["Pride and Prejudice", "Alice's Adventure in Wonderland"];
+    for &book in &to_find {
+        match book_reviews.get(book) {
+            Some(review) => println!("{}: {}", book, review),
+            None => println!("{} is unreviewed.", book)
+        }
+    }
+
+    println!("\n");
+
+    for (book, review) in &book_reviews {
+        println!("{}: \"{}\"", book, review);
+    }
+
 }
